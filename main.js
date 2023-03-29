00e80cc30d5e90eeb6369ebcc808fb8a29e21cdd
Anton Chepko <choptonys@gmail.com>
2023-03-30 00:23:30 +0200

    30.03.2023 updates according to remarks

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let numInput = document.querySelector("#number");
 let monthInput = document.querySelector("#month");
 let yearInput = document.querySelector("#year");
 let cvcInput = document.querySelector("#cvc");
-
+let allInputs = document.querySelectorAll(".formBlock input");
 const cardname = document.querySelector(".card-name");
 const cardnum = document.querySelector(".card-num");
 let carddate = document.querySelector(".card-date");
@@ -15,15 +15,18 @@ const cardcvc = document.querySelector(".card-cvc");
 const thanksDiv = document.querySelector(".thanks")
 const nameDiv = document.querySelector(".name-div")
 const numDiv = document.querySelector(".num-div")
-const dateDive = document.querySelector(".date-div")
+const dateDiv = document.querySelector(".date-div")
 const cvcDiv = document.querySelector(".cvc-div")
 
 let errorSpan = document.createElement("span")
 errorSpan.classList.add("error");
-// errorSpan.classList.remove("grid");
+
 
 const confirmBtn = document.querySelector(".confirm");
 const contBtn = document.querySelector("#cont");
+// \\\\\\\\\\\\\\\\\\\\\\
+console.log(allInputs);
+let errorBlank = document.querySelector('.error2');
 
 nameInput.addEventListener("keyup", (e) => {
     nameDiv.appendChild(errorSpan);
@@ -36,7 +39,7 @@ nameInput.addEventListener("keyup", (e) => {
         if (regex.test(e.target.value)) {
             cardname.textContent = nameInput.value;
             nameDiv.removeChild(errorSpan)
-            nameInput.style.border = '1px solid #eee'
+            nameInput.style.border = '1px solid #06d94b'
         } else {
             errorSpan.innerText = "Incorrect name"
             nameInput.style.border = '1px solid red'
@@ -59,10 +62,10 @@ numInput.addEventListener("keyup", (e) => {
         numInput.style.border = '1px solid #eee';
     } else {
         numInput.value = val.match(split4).join(" ")
-        if (regex.test(val) && val.length === 16) {
+        if (regex.test(val) && val.length <= 16) {
             cardnum.textContent = val.match(split4).join(" ");
             numDiv.removeChild(errorSpan);
-            numInput.style.border = '1px solid #eee'
+            numInput.style.border = '1px solid #06d94b'
         } else {
             errorSpan.innerText = "Incorrect Card Number"
             numInput.style.border = '1px solid red';
@@ -71,19 +74,21 @@ numInput.addEventListener("keyup", (e) => {
 })
 monthInput.addEventListener('keyup', (e) => {
     validater(monthInput);
+
     function validater(input) {
         input.value = input.value.replace(/\D/g, '');
     }
-    dateDive.appendChild(errorSpan);
+
+    dateDiv.appendChild(errorSpan);
     if (monthInput.value.length === 0) {
         cardmonth.innerHTML = '00';
-        dateDive.removeChild(errorSpan);
+        dateDiv.removeChild(errorSpan);
         monthInput.style.border = '1px solid #eee';
     } else {
-        if (parseInt(e.target.value) && parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 12 && e.target.value.match(/(\d)/g).length === e.target.value.length) {
+        if (parseInt(monthInput.value) && parseInt(e.target.value) >= 1 && parseInt(e.target.value) <= 12 && e.target.value.match(/(\d)/g).length === e.target.value.length) {
             e.target.value.length < 2 && parseInt(e.target.value) < 10 ? cardmonth.textContent = `0${e.target.value}` : cardmonth.textContent = e.target.value
-            dateDive.removeChild(errorSpan);
-            numInput.style.border = '1px solid #eee'
+            dateDiv.removeChild(errorSpan);
+            monthInput.style.border = '1px solid #06d94b'
         } else {
             errorSpan.innerText = "Incorrect Month";
             monthInput.style.border = '1px solid red'
@@ -97,13 +102,26 @@ yearInput.addEventListener('keyup', (e) => {
         input.value = input.value.replace(/\D/g, '');
     }
 
-    dateDive.appendChild(errorSpan);
+    dateDiv.appendChild(errorSpan);
     let currentYear = new Date().getFullYear() - 2000;
-    if (parseInt(e.target.value) && parseInt(e.target.value) >= currentYear && e.target.value.match(/(\d)/g).length === e.target.value.length) {
-        e.target.value.length < 2 && parseInt(e.target.value) < 10 ? cardyear.innerText = `0${e.target.value}` : cardyear.innerText = e.target.value
-        dateDive.removeChild(errorSpan);
+    if (yearInput.value.length === 0) {
+        dateDiv.removeChild(errorSpan);
+        cardyear.innerHTML = '00';
+        yearInput.style.border = '1px solid #eee'
     } else {
-        errorSpan.innerText = "InCorrect Year Or Expired Year";
+        if (parseInt(e.target.value) && e.target.value.match(/(\d)/g).length === e.target.value.length) {
+            e.target.value.length < 2 && parseInt(e.target.value) < 10 ? cardyear.innerText = `0${e.target.value}` : cardyear.innerText = e.target.value
+            dateDiv.removeChild(errorSpan);
+            yearInput.style.border = '1px solid #06d94b'
+            if (parseInt(e.target.value) && parseInt(e.target.value) < currentYear) {
+                dateDiv.appendChild(errorSpan);
+                errorSpan.innerText = "Expired year";
+                yearInput.style.border = '1px solid red'
+            }
+        } else {
+            errorSpan.innerText = "Incorrect year or expired year";
+            yearInput.style.border = '1px solid red'
+        }
     }
 })
 cvcInput.addEventListener('keyup', (e) => {
@@ -113,47 +131,50 @@ cvcInput.addEventListener('keyup', (e) => {
         input.value = input.value.replace(/\D/g, '');
     }
 
+    let regex = /^[0-9]+$/g
     cvcDiv.appendChild(errorSpan);
-    if (parseInt(e.target.value) && e.target.value.match(/(\d)/g).length === e.target.value.length) {
-        e.target.value.length < 2 && parseInt(e.target.value) < 10 ? cardcvc.innerText = `0${e.target.value}` : cardcvc.innerText = e.target.value
+    if (cvcInput.value.length === 0) {
+        cardcvc.innerHTML = '000'
         cvcDiv.removeChild(errorSpan);
+        cvcInput.style.border = '1px solid #eee'
     } else {
-        errorSpan.innerText = "InCorrect CVC";
+        if (regex.test(cvcInput.value) && cvcInput.value.length !== 0) {
+            cardcvc.innerHTML = cvcInput.value;
+            cvcDiv.removeChild(errorSpan);
+            cvcInput.style.border = '1px solid #06d94b'
+        } else {
+            errorSpan.innerText = "Incorrect CVC";
+            cvcInput.style.border = '1px solid red'
+        }
     }
 })
+
 confirmBtn.addEventListener('click', (e) => {
     e.preventDefault();
     if (nameInput.value !== '' && numInput.value !== '' && monthInput.value !== '' && yearInput.value !== '' && cvcInput.value !== '') {
         thanksDiv.classList.remove('none');
         form.classList.add('none');
+
     } else {
-        if (nameInput.value === '') {
-            nameDiv.appendChild(errorSpan);
-            errorSpan.innerText = "Can't be blank"
-            nameInput.focus();
-        }
-        if (numInput.value === '') {
-            numDiv.appendChild(errorSpan);
-            errorSpan.innerText = "Can't be blank"
-            numInput.focus();
-        }
-        if (monthInput.value === '') {
-            dateDive.appendChild(errorSpan);
-            errorSpan.innerHTML = `<div>Can't be blank</div>`;
-            monthInput.focus();
-        }
-        if (yearInput.value === '') {
-            dateDive.appendChild(errorSpan);
-            errorSpan.innerHTML = `<div>Can't be blank</div>`;
-            yearInput.focus();
-        }
-        if (cvcInput.value === '') {
-            cvcDiv.appendChild(errorSpan);
-            errorSpan.innerText = "Can't be blank";
-            cvcInput.focus();
-        }
+        allInputs.forEach((item) => {
+            if (item.value === '') {
+
+                const parent = item.parentNode;
+                const errorBlank = parent.querySelector('.error2')
+                errorBlank.innerHTML = "Can't be blank"
+                item.style.border = '1px solid red'
+
+                item.addEventListener('input', () => {
+                    errorBlank.innerHTML = '';
+                    item.style.border = '';
+                });
+            }
+
+        })
     }
 })
+// && (item.style.border = '1px solid red')
+// NodeList(5) [input#name, input#number, input#month, input#year, input#cvc]
 
 contBtn.addEventListener('click', (e) => {
     form.reset();
@@ -163,4 +184,61 @@ contBtn.addEventListener('click', (e) => {
     cardcvc.innerText = "000"
     form.classList.remove("none")
     thanksDiv.classList.add("none")
+    nameInput.style.border = '1px solid #eee'
+    numInput.style.border = '1px solid #eee'
+    monthInput.style.border = '1px solid #eee'
+    yearInput.style.border = '1px solid #eee'
+    cvcInput.style.border = '1px solid #eee'
 })
+//
+// if (nameInput.value === '') {
+//     nameDiv.appendChild(errorSpan);
+//     errorSpan.innerText = "Can't be blank"
+//     nameInput.focus();
+// }
+// if (numInput.value === '') {
+//     numDiv.appendChild(errorSpan);
+//     errorSpan.innerText = "Can't be blank"
+//     numInput.focus();
+// }
+// if (monthInput.value === '') {
+//     dateDiv.appendChild(errorSpan);
+//     errorSpan.innerHTML = `<div>Can't be blank</div>`;
+//     monthInput.focus();
+// }
+// if (yearInput.value === '') {
+//     dateDiv.appendChild(errorSpan);
+//     errorSpan.innerHTML = `<div>Can't be blank</div>`;
+//     yearInput.focus();
+// }
+// if (cvcInput.value === '') {
+//     cvcDiv.appendChild(errorSpan);
+//     errorSpan.innerText = "Can't be blank";
+//     cvcInput.focus();
+// }
+
+
+// nameInput.value === "" ? ( errorSpan.innerText = "sadfsf" && (numInput.style.border = '1px solid red')) :
+//     (numInput.value === "" && (numInput.focus() && (numInput.style.border = '1px solid red')));
+
+
+// nameInput.value === "" ? (nameInput.focus() && (numInput.style.border = '1px solid red')):
+//     (numInput.value == "" ? (numInput.focus() && (numInput.style.border = '1px solid red'))):
+// (monthInput.value == "" ?(monthInput.focus() && (monthInput.style.border = '1px solid red'))) :
+// yearInput.value == "" ? (yearInput.focus() && (yearInput.style.border = '1px solid red')) :
+// (cvcInput.focus() && (cvcInput.style.border = '1px solid red'))
+
+
+// allInputs.forEach((item) => {
+//     // item.appendChild(errorSpan);
+//     item.style.border = '1px solid red'
+//     errorSpan.innerHTML = "Can't be blank"
+// }
+// )
+
+
+// item.id === 'name' ? nameDiv.span.innerHTML = "Can't be blank" :
+// item.id === 'number' ?numDiv.span.innerHTML = "Can't be blank" :
+// item.id === 'month' ? dateDiv.span.innerHTML = "Can't be blank" :
+// item.id === 'year' ? dateDiv.span.innerHTML = "Can't be blank" :
+//     cvcDiv.span.innerHTML = "Can't be blank";
